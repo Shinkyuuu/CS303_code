2f6221611658988005ccac54ad192d1c54e540d6
Shinkyuuu <codyspark@gmail.com>
2021-03-21 14:23:55 -0700

    Added comments

--- homework6.py
+++ homework6.py
@@ -1,31 +1,49 @@
+# Find a number raised to a powe of 2 while modulating it every step.
 def powMod(a, c, iterator, counter=0):
     if  counter >= iterator:
         return a
    
     num1 = (a ** 2) % c
 
-    #print("Iteration: " + str(counter + 1) + " is " + str(num1))
+    # Print the outcome of every iteration
+    # (Program runs faster when commented out)
+    print("Iteration: " + str(counter + 1) + " is " + str(num1))
 
     return powMod(num1, c, iterator, counter+1)
 
 
-def factorPow(a, c, binList):
+# If the c isn't a power of 2, split it into powers of 2 and multiply results together.
+def factorPow(a, c, bList):
+    #bigPow = number raised to the power of powers of 2 multiplied together.
     bigPow = 1
 
-    for i in range(len(binList)):
-        if  binList[i] != 0:
+    # For the length of the binary list, raise a to the power of 2 as many
+    # times as its index..
+    for i in range(len(bList)):
+        if  bList[i] != 0:
             bigPow = (bigPow * powMod(a, c, i)) % c
-            #print()
+            print()
     return bigPow
 
-def userInput(a, b, c):
-    binList = [int(i) for i in str(bin(b))[2:]]
-    binList.reverse()
-    finalNum = factorPow(a, c, binList)
+
+# Values are inputted and formatted into something more workable.
+def userInput():
+    # a = number to be exponentiated and modulated.
+    # b = the power.
+    # c = the moduli.
+    a = int(input("a: "))
+    b = int(input("b: "))
+    c = int(input("c: "))
+
+    # If a binary number is converted into a list and then reversed, the powers of 
+    # 2 that correspond to the binary number are equal to the index of each 
+    # element in the list.
+    bList = [int(i) for i in str(bin(b))[2:]]
+    bList.reverse()
+    finalNum = factorPow(a, c, bList)
 
     return finalNum
 
 
-print(userInput(243432243324234523254634576783456784523678456257656565654345345346765789678678678567454345234423437456327845623784567328645475745, 994326745675439687435768345786437643578643578634578643554768435786345786345786345678345786345897439943432243234234234234324342324999, 54234233244322432678324786234768324786343243423243242344))
+print("Solution is " + str(userInput()))
 
-#print((999999 ** 999999) % 23)
